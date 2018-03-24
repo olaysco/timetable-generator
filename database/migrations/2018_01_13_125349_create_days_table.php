b19335c16e9f26110c3fcf1a44746e4782299bb0
Richman Larry CLifford <larriche99@gmail.com>
2018-03-24 18:59:14 +0000

    Updates to migrations
    - Added new field to days table

--- database/migrations/2018_01_13_125349_create_days_table.php
+++ database/migrations/2018_01_13_125349_create_days_table.php
@@ -16,6 +16,7 @@ class CreateDaysTable extends Migration
         Schema::create('days', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->string('short_name');
             $table->timestamps();
         });
     }
