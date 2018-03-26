4ea66213096db590ec43a1da4b7ccc8044ba321f
Richman Larry CLifford <larriche99@gmail.com>
2018-03-26 00:00:59 +0000

    Updates to timeslots module
    - Added all CRUD methods for timeslots controller

--- app/Http/Controllers/TimeslotsController.php
+++ app/Http/Controllers/TimeslotsController.php
@@ -92,12 +92,76 @@ class TimeslotsController extends Controller
 
         if ($timeslot) {
             $timeParts = explode("-", $timeslot->time);
-            $timeslot->from = $timeParts[0];
-            $timeslot->to = $timeParts[1];
+            $timeslot->from = trim($timeParts[0]);
+            $timeslot->to = trim($timeParts[1]);
 
             return Response::json($timeslot, 200);
         } else {
             return Response::json(['error' => 'Timeslot not found'], 404);
+        }
+    }
+
+    /**
+     * Update the timeslot with the given Id
+     *
+     * @param int $id The id of the timeslot to update
+     * @param Illuminat\Http\Request $request The HTTP request
+     */
+    public function update($id, Request $request)
+    {
+        $timeslot = Timeslot::find($id);
+
+        if (!$timeslot) {
+            return Response::json(['errors' => ['Timeslot not found']], 404);
+        }
+
+        $rules = [
+            'from' => 'required|before:to',
+            'to' => 'required|after:from'
+        ];
+
+        $messages = [
+            'from.before' => 'From time must be before To time',
+            'to.after' => 'To time must be after From time'
+        ];
+
+        $this->validate($request, $rules, $messages);
+
+        $exists = Timeslot::where('time', Timeslot::createTimePeriod($request->from, $request->to))
+            ->where('id', '<>', $id)
+            ->first();
+
+        if ($exists) {
+            return Response::json(['errors' => ['This timeslot already exists']], 422);
+        }
+
+        $data = $request->all();
+        $data['time'] = Timeslot::createTimePeriod($data['from'], $data['to']);
+
+        if ($this->service->update($id, $data)) {
+            return Response::json(['message' => 'Timeslot updated'], 200);
+        }
+
+        return Response::json(['error' => 'A system error occurred'], 500);
+    }
+
+    /**
+     * Delete the timeslot with the given id
+     *
+     * @param int $id The id of the timeslot to delete
+     */
+    public function destroy($id)
+    {
+        $timeslot = Timeslot::find($id);
+
+        if (!$timeslot) {
+            return Response::json(['error' => 'Timeslot not found'], 404);
+        }
+
+        if ($this->service->delete($id)) {
+            return Response::json(['message' => 'Timeslot has been deleted'], 200);
+        } else {
+            return Response::json(['error' => 'An unknown system error occurred'], 500);
         }
     }
 }
