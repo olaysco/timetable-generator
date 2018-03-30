e4ded5295baf5d0294a201562925d4ac7397e127
Richman Larry CLifford <larriche99@gmail.com>
2018-03-30 00:30:03 +0000

    Updates to authentication and user management module
    - Implemented user activation controller method
    - Replaced overwritten app layout file

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -22,4 +22,44 @@ class UsersController extends Controller
 
         return view('users.activate', compact('user', 'questions'));
     }
+
+    /**
+     * Activate and set up account for user
+     *
+     * @param Illuminate\Http\Request $request The HTTP request
+     * @return Illuminate\Http\Response Redirect to home page
+     */
+    public function activateUser(Request $request)
+    {
+        $user = Auth::user();
+
+        if ($user->activated) {
+            return redirect()->back()->withError('Your account is already activated');
+        }
+
+        $rules = [
+            'username' => 'required|unique:users,username,' . $user->id,
+            'name' => 'required',
+            'password' => 'required|confirmed',
+            'security_question_id' => 'required|exists:security_questions,id',
+            'security_question_answer' => 'required'
+        ];
+
+        $messages = [
+            'security_question_id.required' => 'A security question must be selected.',
+            'security_question_answer.required' => 'Add an answer for security question.'
+        ];
+
+        $this->validate($request, $rules, $messages);
+
+        $user->update([
+            'username' => $request->username,
+            'name' => 'required',
+            'password' => bcrypt($request->password),
+            'security_question_id' => $request->security_question_id,
+            'security_question_answer' => $request->security_question_answer
+        ]);
+
+        return redirect('/');
+    }
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,80 +1,31 @@
 <!DOCTYPE html>
-<html lang="{{ app()->getLocale() }}">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<html lang="en">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+		<!-- Styles -->
+		@include('partials.styles')
+		@yield('styles')
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+		<title>@yield('title') | Timetable</title>
+    </head>
 
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-</head>
-<body>
-    <div id="app">
-        <nav class="navbar navbar-default navbar-static-top">
-            <div class="container">
-                <div class="navbar-header">
+    <body>
+        <div class="container-fluid">
+            <div class="row">
+                @include('partials.sidebar')
 
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse" aria-expanded="false">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-
-                    <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
-                    </a>
-                </div>
-
-                <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                        &nbsp;
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="nav navbar-nav navbar-right">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li><a href="{{ route('login') }}">Login</a></li>
-                            <li><a href="{{ route('register') }}">Register</a></li>
-                        @else
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <ul class="dropdown-menu">
-                                    <li>
-                                        <a href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            Logout
-                                        </a>
-
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
+                @yield('content')
             </div>
-        </nav>
+        </div>
 
-        @yield('content')
-    </div>
+        @include('partials.confirm_dialog')
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
-</body>
+        <!-- Scripts -->
+        @include('partials.scripts')
+        @yield('scripts')
+    </body>
 </html>
